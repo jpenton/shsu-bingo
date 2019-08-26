887964185e0a16574f197630bb3cf52ec066bc8b
Jonathan Penton <jonathan@penton.site>
2019-08-26 16:43:29 -0500

    Display title when cardId is available

--- pages/cards/[cardId].tsx
+++ pages/cards/[cardId].tsx
@@ -3,11 +3,7 @@ import { useRouter } from "next/dist/client/router";
 function CardPage() {
   const { cardId } = useRouter().query;
 
-  return (
-    <>
-      <h1>{`Dr. ${cardId} Bingo Card`}</h1>
-    </>
-  );
+  return <>{cardId ? <h1>{`Dr. ${cardId} Bingo Card`}</h1> : null}</>;
 }
 
 export default CardPage;
