a2909c4a918e81932a4eb0e1d3625d98dbaba992
Jonathan Penton <jonathan@penton.site>
2019-08-28 09:54:54 -0500

    Move populateCard code to useEffect
    Code only runs on profName change

--- pages/card.tsx
+++ pages/card.tsx
@@ -1,7 +1,7 @@
 import { useRouter } from "next/dist/client/router";
 import Head from "next/head";
-import React from 'react';
-import _ from 'lodash';
+import React from "react";
+import _ from "lodash";
 
 interface IBingoCell {
   text: string;
@@ -12,49 +12,55 @@ interface IBingoCard {
   freeImage: string;
   spaces: IBingoCell[];
 }
-const CellStrings: Record<'Burris' | 'Cho', string[]> & {[key: string]: string[]} = { 
-  Burris: ['string1', 'string2'],
-  Cho: ['cho1', 'cho2']
-}
-const freeImages = ['bur', 'cho'];
+const CellStrings: Record<"Burris" | "Cho", string[]> & {
+  [key: string]: string[];
+} = {
+  Burris: ["string1", "string2"],
+  Cho: ["cho1", "cho2"]
+};
+const freeImages = ["bur", "cho"];
 
 interface IBingoCardProps {
   profName: string;
 }
 
-function BingoCard({profName}: IBingoCardProps) {
-  
-  const cellCopy = [...CellStrings[profName]];
-  const populatedCard = [];
+function BingoCard({ profName }: IBingoCardProps) {
+  const [cells, setCells] = React.useState([]);
 
-  for (var i = 0 ; i < 25 ; i++) {
-    if (i === 12) {
-      populatedCard.push(null);
-      continue;
+  React.useEffect(() => {
+    if (!profName) {
+      return;
     }
-    const randNum = Math.floor(Math.random()*cellCopy.length)
-    const randIsm = cellCopy.splice(randNum, 1);
-    populatedCard.push(randIsm);
-  }
 
-  const [cells, setCells] = React.useState(populatedCard);
+    const cellCopy = [...CellStrings[profName]];
+    const populatedCard = [];
 
-  return(
+    for (var i = 0; i < 25; i++) {
+      if (i === 12) {
+        populatedCard.push(null);
+        continue;
+      }
+      const randNum = Math.floor(Math.random() * cellCopy.length);
+      const randIsm = cellCopy.splice(randNum, 1);
+      populatedCard.push(randIsm);
+    }
+
+    setCells(populatedCard);
+  }, [profName]);
+
+  return (
     <>
-    {_.chunk(cells, 5).map((row, index) => (
-      <div className = 'bingo-row'
-      key = {index}>
-      {row.map((cell, index) => (
-        <div className = 'bingo-cell'
-        key = {index}>
-          {cell}
+      {_.chunk(cells, 5).map((row, index) => (
+        <div className="bingo-row" key={index}>
+          {row.map((cell, index) => (
+            <div className="bingo-cell" key={index}>
+              {cell}
+            </div>
+          ))}
         </div>
       ))}
-      </div>
-    ))}
     </>
-  )
-
+  );
 }
 
 function CardPage() {
@@ -65,8 +71,9 @@ function CardPage() {
       <Head>
         <title>{`${name ? `Dr. ${name}` : "Bingo Card"} | SHSU Bingo`}</title>
       </Head>
-      {name ? <h1>{`Dr. ${name} Bingo Card`}</h1> : null}
-      <BingoCard profName = {Array.isArray(name)? name[0] : name} />
+      <h1>{name ? `Dr. ${name} ` : null}Bingo Card</h1>
+      {/* {name ? <h1>{`Dr. ${name} Bingo Card`}</h1> : null} */}
+      <BingoCard profName={Array.isArray(name) ? name[0] : name} />
     </>
   );
 }
