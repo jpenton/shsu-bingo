d8b690545a3147048ebd96fb22e5cad9bdf184c2
QuaffleCaresser <ssmiffie@gmail.com>
2019-08-29 17:24:52 -0500

    added burrisisms

--- pages/card.tsx
+++ pages/card.tsx
@@ -15,7 +15,13 @@ interface IBingoCard {
 const CellStrings: Record<"Burris" | "Cho", string[]> & {
   [key: string]: string[];
 } = {
-  Burris: ["string1", "string2"],
+  Burris: ["\"Professional Grade!!\"", "\"Mickey Mouse Programming\"", "\"Are you/we satisfied??\"", "5x \"Deal\" [ ][ ][ ][ ][ ]", "\"Wrong side of the tracks\"",
+            "\"Road map fee\"", "\"Donald Knuth\"", "\"Garbage Collection\"", "\"The Snake\" / \"The Squeeze\"", "\"It's ethical!!\"", 
+            "\"I've got time\"", "\"Let's see if we can't...\"", "\"Are you kidding me??\"", "\"Back when I worked with NASA...\"",
+            "\"Do your competition a favor!\"", "\"The Hymnal\"", "*anything about DF majors being untrustworthy*", "\"Kiss the pavement\"",
+            "*something about assembly being your favorite language*", "*something about Apollo 11 having 2k of storage / 32k memory*",
+            "\"If you haven't started on your lab...\"", "\"If you do the *x* option, you should be able to do the *y* option.\"",
+            "\"In Ada...\"", "\"Fruit of the Loom\""],
   Cho: ["cho1", "cho2"]
 };
 const freeImages = ["bur", "cho"];
