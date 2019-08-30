9e5cc0b389c6eaf20953cd92158a59858183df7c
QuaffleCaresser <ssmiffie@gmail.com>
2019-08-29 19:56:52 -0500

    implemented freespace image

--- pages/card.tsx
+++ pages/card.tsx
@@ -5,50 +5,66 @@ import _ from "lodash";
 
 interface IBingoCell {
   text: string;
-  marked: boolean;
+  marked?: boolean;
+  isImage?: boolean;
 }
 
-interface IBingoCard {
-  freeImage: string;
-  spaces: IBingoCell[];
-}
-const CellStrings: Record<"Burris" | "Cho", string[]> & {
-  [key: string]: string[];
+const CellStrings: Record<"Burris" | "Cho", IBingoCell[]> & {
+  [key: string]: IBingoCell[];
 } = {
-  Burris: ["\"Professional Grade!!\"", "\"Mickey Mouse Programming\"", "\"Are you/we satisfied??\"", "5x \"Deal\" [ ][ ][ ][ ][ ]", "\"Wrong side of the tracks\"",
-            "\"Road map fee\"", "\"Donald Knuth\"", "\"Garbage Collection\"", "\"The Snake\" / \"The Squeeze\"", "\"It's ethical!!\"", 
-            "\"I've got time\"", "\"Let's see if we can't...\"", "\"Are you kidding me??\"", "\"Back when I worked with NASA...\"",
-            "\"Do your competition a favor!\"", "\"The Hymnal\"", "*anything about DF majors being untrustworthy*", "\"Kiss the pavement\"",
-            "*something about assembly being your favorite language*", "*something about Apollo 11 having 2k of storage / 32k memory*",
-            "\"If you haven't started on your lab...\"", "\"If you do the *x* option, you should be able to do the *y* option.\"",
-            "\"In Ada...\"", "\"Fruit of the Loom\""],
-  Cho: ["cho1", "cho2"]
+  Burris: [
+            { text: "Professional Grade!!" },
+            { text: "\"Mickey Mouse Programming\"" },
+            { text: "\"Are you/we satisfied??\"" },
+            { text: "5x \"Deal\" [ ][ ][ ][ ][ ]" },
+            { text: "\"Wrong side of the tracks\"" },
+            { text: "\"Road map fee\"" },
+            { text: "\"Donald Knuth\"" },
+            { text: "\"Garbage Collection\"" },
+            { text: "\"The Snake\" / \"The Squeeze\"" },
+            { text: "\"It's ethical!!\"" },
+            { text: "\"I've got time\"" },
+            { text: "\"Let's see if we can't...\"" },
+            { text: '/static/profHat.png', isImage: true },
+            { text: "\"Are you kidding me??\"" },
+            { text: "\"Back when I worked with NASA...\"" },
+            { text: "\"Do your competition a favor!\"" },
+            { text: "\"The Hymnal\"" },
+            { text: "*anything about DF majors being untrustworthy*" },
+            { text: "\"Kiss the pavement\"" },
+            { text: "*something about assembly being your favorite language*" },
+            { text: "*something about Apollo 11 having 2k of storage / 32k memory*" },
+            { text: "\"If you haven't started on your lab...\"" },
+            { text: "\"If you do the *x* option, you should be able to do the *y* option.\"" },
+            { text: "\"In Ada...\"" },
+            { text: "\"Fruit of the Loom\"" },
+          ],
+  Cho: []
 };
-const freeImages = ["bur", "cho"];
 
 interface IBingoCardProps {
   profName: string;
 }
 
 function BingoCard({ profName }: IBingoCardProps) {
-  const [cells, setCells] = React.useState([]);
+  const [cells, setCells] = React.useState<IBingoCell[]>([]);
 
   React.useEffect(() => {
     if (!profName) {
-      return;
+      return; 
     }
 
-    const cellCopy = [...CellStrings[profName]];
+    const cellCopy = [...CellStrings[profName]].filter((cell) => !cell.isImage);
     const populatedCard = [];
 
     for (var i = 0; i < 25; i++) {
       if (i === 12) {
-        populatedCard.push(null);
+        populatedCard.push(CellStrings[profName][12]);
         continue;
       }
       const randNum = Math.floor(Math.random() * cellCopy.length);
       const randIsm = cellCopy.splice(randNum, 1);
-      populatedCard.push(randIsm);
+      populatedCard.push(randIsm[0]);
     }
 
     setCells(populatedCard);
@@ -60,12 +76,12 @@ function BingoCard({ profName }: IBingoCardProps) {
         <div className="bingo-row" key={index}>
           {row.map((cell, index) => (
             <div className="bingo-cell" key={index}>
-              {cell}
+              {cell.isImage ? (<img src= {cell.text} style = {{width: '100%'}}/>) : <span style= {{textAlign: 'center'}}> {cell.text} </span>}
             </div>
           ))}
         </div>
       ))}
-    </>
+    </> 
   );
 }
 
@@ -78,7 +94,6 @@ function CardPage() {
         <title>{`${name ? `Dr. ${name}` : "Bingo Card"} | SHSU Bingo`}</title>
       </Head>
       <h1>{name ? `Dr. ${name} ` : null}Bingo Card</h1>
-      {/* {name ? <h1>{`Dr. ${name} Bingo Card`}</h1> : null} */}
       <BingoCard profName={Array.isArray(name) ? name[0] : name} />
     </>
   );
