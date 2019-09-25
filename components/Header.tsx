522ebf8d3b50bb6645b231071170c76233bb0a95
Jonathan Penton <jonathan@penton.site>
2019-09-25 16:41:18 -0500

    Link to repo
    So other people can request changes
    Features, isms, etc..

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,10 +4,11 @@ import Container from './Container';
 function Header() {
   return (
     <Container className="bg-cool-grey-800 py-3">
-      <Link href="/">
-        <a className="text-xl text-white font-medium">SHSU Bingo</a>
-      </Link>
-      {/* <Nav className="ml-auto" navbar>
+      <div className="flex justify-between items-baseline">
+        <Link href="/">
+          <a className="text-xl text-white font-medium">SHSU Bingo</a>
+        </Link>
+        {/* <Nav className="ml-auto" navbar>
           <NavItem>
             <NavLink href="/components/">Components</NavLink>
           </NavItem>
@@ -17,6 +18,17 @@ function Header() {
             </NavLink>
           </NavItem>
         </Nav> */}
+        <div>
+          <a
+            className="text-lg text-white"
+            href="https://github.com/jpenton/shsu-bingo/issues"
+            target="_blank"
+            rel="noopener"
+          >
+            Open Issues
+          </a>
+        </div>
+      </div>
     </Container>
   );
 }
